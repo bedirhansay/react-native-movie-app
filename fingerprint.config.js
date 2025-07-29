cf4330a95c88d7e17d85efcebfe5ded2d95b3e43
Bedirhan SAY <bedirhan.sayy@gmail.com>
2025-07-29 09:11:00 +0300

    feat(payable-aging): refactor

--- fingerprint.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@expo/fingerprint').Config} */
-const config = {
-  sourceSkips: [
-    'ExpoConfigRuntimeVersionIfString',
-    'ExpoConfigVersions',
-    'PackageJsonAndroidAndIosScriptsIfNotContainRun',
-  ],
-};
-module.exports = config;
